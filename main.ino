00f31dd049e1d50a8ab0db9e794f7da647b64101
Thomas Eastman <tjeastman@gmail.com>
2016-02-20 20:19:07 -0800

    read sensor values from MPU-6050 I2C device

--- main.ino
+++ main.ino
@@ -1,12 +1,15 @@
 #include <SPI.h>
+#include <Wire.h>
 #include "RF24.h"
+
+const byte MPU6050_ADDRESS = 0x68; // I2C address of the MPU-6050 device
 
 const rf24_pa_dbm_e RADIO_POWER_LEVEL = RF24_PA_LOW;  // default is RF24_PA_MAX
 const rf24_datarate_e RADIO_DATA_RATE = RF24_1MBPS; //RF24_250KBPS;
 
 RF24 radio(7, 8);  // set up nRF24L01 radio on SPI bus plus pins 7 & 8
 
-const unsigned long SEND_DELAY_MS = 1000;
+const unsigned long SEND_DELAY_MS = 500;
 
 enum radio_role_e {
   sender,
@@ -15,16 +18,51 @@ enum radio_role_e {
 
 const radio_role_e radio_role = RADIO_ROLE;
 
-typedef unsigned long sensor_val_t;
+struct sensor_val_t {
+  int accel_X;
+  int accel_Y;
+  int accel_Z;
+  float temp;
+  int gyro_X;
+  int gyro_Y;
+  int gyro_Z;
+};
 
-unsigned long read_sensor()
+sensor_val_t read_sensor()
 {
-  return micros();
+  sensor_val_t sensor_val;
+
+  // read values from the MPU-6050 device
+  Wire.beginTransmission(MPU6050_ADDRESS);
+  Wire.write(0x3B);  // start with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU6050_ADDRESS, 14, true);  // request a total of 14 registers
+
+  int temp;
+  sensor_val.accel_X = Wire.read() << 8 | Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  sensor_val.accel_Y = Wire.read() << 8 | Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  sensor_val.accel_Z = Wire.read() << 8 | Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  temp = Wire.read() << 8 | Wire.read();                // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  sensor_val.gyro_X = Wire.read() << 8 | Wire.read();   // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  sensor_val.gyro_Y = Wire.read() << 8 | Wire.read();   // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  sensor_val.gyro_Z = Wire.read() << 8 | Wire.read();   // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+  sensor_val.temp = (float)temp / 340.00 + 36.53;
+
+  return sensor_val;
 }
 
 void setup()
 {
   Serial.begin(115200);
+
+  if (radio_role == sender) {
+    Wire.begin();
+    Wire.beginTransmission(MPU6050_ADDRESS);
+    Wire.write(0x6B);  // PWR_MGMT_1 register
+    Wire.write(0);     // set to zero to wake up the MPU-6050
+    Wire.endTransmission(true);
+  }
 
   radio.begin();
 
@@ -68,7 +106,7 @@ void loop()
 
     sensor_val = read_sensor();
     Serial.print(F("sending sensor value: "));
-    Serial.print(sensor_val);
+    Serial.print(sensor_val.temp);
     Serial.print(F("..."));
 
     if (!radio.write(&sensor_val, sizeof(sensor_val_t))){
@@ -90,7 +128,7 @@ void loop()
       }
 
       Serial.print(F("received sensor value: "));
-      Serial.println(sensor_val);
+      Serial.println(sensor_val.temp);
     }
   }
 }
