c1be6d54822571451a7a672658ec49402a6a892c
Thomas Eastman <tjeastman@gmail.com>
2016-02-20 20:22:20 -0800

    exit if upload fails to receiving device

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@ export ARDUINO_PORT1=/dev/ttyACM0
 export ARDUINO_PORT2=/dev/ttyUSB0
 
 make clean
-make RADIO_ROLE=receiver && make upload ARDUINO_PORT=$ARDUINO_PORT1
+make RADIO_ROLE=receiver && make upload ARDUINO_PORT=$ARDUINO_PORT1 || exit 1
 make clean
 make RADIO_ROLE=sender && make upload ARDUINO_PORT=$ARDUINO_PORT2
 make monitor ARDUINO_PORT=/dev/ttyUSB0
