1097935ed9a1e6f4f85d32ba098ecbc70266714a
Thomas Eastman <tjeastman@gmail.com>
2016-02-20 21:09:01 -0800

    move sensor-specific functionality into separate file

--- main.ino
+++ main.ino
@@ -3,8 +3,6 @@
 #include "RF24.h"
 #include "sensor.h"
 
-const byte MPU6050_ADDRESS = 0x68; // I2C address of the MPU-6050 device
-
 const rf24_pa_dbm_e RADIO_POWER_LEVEL = RF24_PA_LOW;  // default is RF24_PA_MAX
 const rf24_datarate_e RADIO_DATA_RATE = RF24_1MBPS; //RF24_250KBPS;
 
@@ -19,40 +17,12 @@ enum radio_role_e {
 
 const radio_role_e radio_role = RADIO_ROLE;
 
-sensor_val_t read_sensor()
-{
-  sensor_val_t sensor_val;
-
-  // read values from the MPU-6050 device
-  Wire.beginTransmission(MPU6050_ADDRESS);
-  Wire.write(0x3B);  // start with register 0x3B (ACCEL_XOUT_H)
-  Wire.endTransmission(false);
-  Wire.requestFrom(MPU6050_ADDRESS, 14, true);  // request a total of 14 registers
-
-  int temp;
-  sensor_val.accel_X = Wire.read() << 8 | Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
-  sensor_val.accel_Y = Wire.read() << 8 | Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
-  sensor_val.accel_Z = Wire.read() << 8 | Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
-  temp = Wire.read() << 8 | Wire.read();                // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
-  sensor_val.gyro_X = Wire.read() << 8 | Wire.read();   // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
-  sensor_val.gyro_Y = Wire.read() << 8 | Wire.read();   // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
-  sensor_val.gyro_Z = Wire.read() << 8 | Wire.read();   // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
-
-  sensor_val.temp = (float)temp / 340.00 + 36.53;
-
-  return sensor_val;
-}
-
 void setup()
 {
   Serial.begin(115200);
 
   if (radio_role == sender) {
-    Wire.begin();
-    Wire.beginTransmission(MPU6050_ADDRESS);
-    Wire.write(0x6B);  // PWR_MGMT_1 register
-    Wire.write(0);     // set to zero to wake up the MPU-6050
-    Wire.endTransmission(true);
+    initialize_sensor();
   }
 
   radio.begin();
---
+++ sensor.cpp
@@ -0,0 +1,36 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include "sensor.h"
+
+void initialize_sensor()
+{
+  Wire.begin();
+  Wire.beginTransmission(MPU6050_ADDRESS);
+  Wire.write(0x6B);  // PWR_MGMT_1 register
+  Wire.write(0);     // set to zero to wake up the MPU-6050
+  Wire.endTransmission(true);
+}
+
+sensor_val_t read_sensor()
+{
+  sensor_val_t sensor_val;
+
+  // read values from the MPU-6050 device
+  Wire.beginTransmission(MPU6050_ADDRESS);
+  Wire.write(0x3B);  // start with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  Wire.requestFrom(MPU6050_ADDRESS, 14, true);  // request a total of 14 registers
+
+  int temp;
+  sensor_val.accel_X = Wire.read() << 8 | Wire.read();  // 0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)
+  sensor_val.accel_Y = Wire.read() << 8 | Wire.read();  // 0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  sensor_val.accel_Z = Wire.read() << 8 | Wire.read();  // 0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  temp = Wire.read() << 8 | Wire.read();                // 0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  sensor_val.gyro_X = Wire.read() << 8 | Wire.read();   // 0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  sensor_val.gyro_Y = Wire.read() << 8 | Wire.read();   // 0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  sensor_val.gyro_Z = Wire.read() << 8 | Wire.read();   // 0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+
+  sensor_val.temp = (float)temp / 340.00 + 36.53;
+
+  return sensor_val;
+}
