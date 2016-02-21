531c9b943f9418a288b074dd2a7a86435cb33d8f
Thomas Eastman <tjeastman@gmail.com>
2016-02-20 20:22:00 -0800

    move port devices to top

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
+export ARDUINO_PORT1=/dev/ttyACM0
+export ARDUINO_PORT2=/dev/ttyUSB0
+
 make clean
-make RADIO_ROLE=receiver && make upload ARDUINO_PORT=/dev/ttyACM0
+make RADIO_ROLE=receiver && make upload ARDUINO_PORT=$ARDUINO_PORT1
 make clean
-make RADIO_ROLE=sender && make upload ARDUINO_PORT=/dev/ttyUSB0
+make RADIO_ROLE=sender && make upload ARDUINO_PORT=$ARDUINO_PORT2
 make monitor ARDUINO_PORT=/dev/ttyUSB0
